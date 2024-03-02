d4225687a49d523d465708c4539b0126abb283c0
Quentin Armitage <quentin@armitage.org.uk>
2024-03-02 15:09:21 +0000

    Report options 12 and 81 in sent packet
    Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

--- pkt4_send.cpp
+++ pkt4_send.cpp
@@ -19,19 +19,23 @@ int pkt4_send(CalloutHandle& handle) {
 	// use a try...catch block here because if the client was not interesting,
 	// no information would be set and getArgument would thrown an exception.
 	string hwaddr;
+	Pkt4Ptr response4_ptr;
 	try {
 		handle.getContext("hwaddr", hwaddr);
  
 		// getContext didn't throw so the client is interesting.  Get a pointer
 		// to the reply.
-		Pkt4Ptr response4_ptr;
 		handle.getArgument("response4", response4_ptr);
  
 		// Get the string form of the IP address.
 		string ipaddr = response4_ptr->getYiaddr().toText();
  
 		// Write the information to the log file.
-		interesting << hwaddr << " " << ipaddr << "\n";
+		interesting << hwaddr << " " << ipaddr <<
+			" Ci " << response4_ptr->getCiaddr().toText() <<
+			" Gi " << response4_ptr->getGiaddr().toText() <<
+			" Si " << response4_ptr->getSiaddr().toText() <<
+			"\n";
  
 		// ... and to guard against a crash, we'll flush the output stream.
 		flush(interesting);
@@ -42,6 +46,18 @@ int pkt4_send(CalloutHandle& handle) {
 		// and dismiss the exception.
 	 }
  
+	OptionPtr opt12 = response4_ptr->getOption(12);
+	if (opt12)
+		interesting << "opt12 " << opt12->toString() << ": " << opt12->toText() << "\n";
+	else
+		interesting << "no option 12\n";
+	OptionPtr opt81 = response4_ptr->getOption(81);
+	if (opt81)
+		interesting << "opt81 " << opt81->toString() << ": " << opt81->toText() << "\n";
+	else
+		interesting << "no option 81\n";
+	flush(interesting);
+
 	return (0);
 }
  
